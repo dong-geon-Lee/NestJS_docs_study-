ac58a2f7295c88f499fbb7ccedcc73a4f8c058de
developer <pre8248@gmail.com>
2023-09-28 10:25:56 +0900

    cats 컨트롤러 및 서비스 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatService } from './cats.service';
+import { CatsController } from './cats.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, CatsController],
+  providers: [AppService, CatService],
 })
 export class AppModule {}
