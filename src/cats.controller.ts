1d63b4cc29eef4b911ff1ff660fc6c8ddd330750
developer <pre8248@gmail.com>
2023-09-28 11:46:42 +0900

    Post 메서드 및 리다이렉트 정의

--- src/cats.controller.ts
+++ src/cats.controller.ts
@@ -1,12 +1,45 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Header,
+  HttpCode,
+  Post,
+  Query,
+  Redirect,
+  Req,
+} from '@nestjs/common';
 import { CatService } from './cats.service';
+import { Request } from 'express';
 
-@Controller('cats')
+@Controller('api/cats')
 export class CatsController {
   constructor(private readonly catService: CatService) {}
 
-  @Get()
+  @Get('/')
   findAll(): string {
     return this.catService.findAll();
+  }
+
+  @Get('/love')
+  @Redirect('https://nestjs.com')
+  LoveCat(@Req() req: Request): string {
+    console.log(req.body);
+    return this.catService.LoveCat();
+  }
+
+  @Post('/add')
+  @Header('Cache-Control', 'none')
+  @HttpCode(204)
+  create(): string {
+    return 'This action adds a new cat';
+  }
+
+  @Get('/docs')
+  @Redirect('https://docs.nestjs.com', 302)
+  getDocs(@Query('version') version) {
+    if (version && version === '5') {
+      console.log(`${version} 맞아`);
+      return { url: 'https://docs.nestjs.com/v5/' };
+    }
   }
 }
